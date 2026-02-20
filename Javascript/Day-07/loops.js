ab9525bf8c4c9c8e6fb7fbce8ffd5ff76d70406c
Mahadiur <almahadiurrahman@gmail.com>
2026-02-20 10:01:44 -0800

    7 days half work is complete

---
+++ Javascript/Day-07/loops.js
@@ -0,0 +1,20 @@
+// Normal Loop
+
+for (let i = 1; i <= 10; i++){
+    console.log(i); // 1 to 10
+}
+
+
+// List Item
+let fruit_list = ['apple', 'banana', 'orange', 'Kiwi'];
+console.log(fruit_list[0]);
+
+for (let i = 0; i < fruit_list.length; i++){
+    console.log(fruit_list[i]); // show index wise
+}
+
+
+// reverse loop
+for (let x = 10; x >= 1; x--){
+    console.log(x)
+}
