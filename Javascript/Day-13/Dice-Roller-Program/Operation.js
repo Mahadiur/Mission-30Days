c1420145d2df97c2a28df67cf1ab34c43b166681
= <=>
2026-03-11 06:56:10 -0700

    Complete Dice Roller Program

--- Javascript/Day-13/Dice-Roller-Program/Operation.js
+++ Javascript/Day-13/Dice-Roller-Program/Operation.js
@@ -1,1 +1,14 @@
-const num = 0;
+const userinput = document.querySelector('#userinput');
+const randomdicenum = document.querySelector('#randomdicenum');
+const resultimagehum = document.querySelector('.resultimagehum');
+const resultimagecom = document.querySelector('.resultimagecom');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', ()=>{
+    valueofinput = userinput.value;
+    Genrandomnum = Math.floor(Math.random() * (6 -1) + 1);
+    randomdicenum.innerText = `${valueofinput} and ${Genrandomnum}`;
+    resultimagehum.innerHTML = `<img src='./Dice-Images/${valueofinput}.png'>`
+    resultimagecom.innerHTML = `<img src='./Dice-Images/${Genrandomnum}.png'>`
+
+})
